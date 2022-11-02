49364a7529ba47c4fedc6d2161ba3f293aeb3ba3
santiago1230 <santiago@bluecaribu.com>
2022-11-01 20:06:29 -0500

    [register] useRef

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,6 @@
 import React, {useRef} from 'react';
 import { useNavigate, useLocation, Link, NavLink } from 'react-router-dom';
+import { sendForm } from './Peticiones';
 
 
 export default function register()
@@ -8,40 +9,68 @@ export default function register()
     const clickbtn = () => navigate("/sistema/datos");
     const pathname = useLocation().pathname;
 
+    //-- referencias user
+    const refUser = useRef(null);
+    const refCC = useRef(null);
+    const refEmail = useRef(null);
+    const refPass = useRef(null);
+
+    //-- referencias user
+    const refMarca = useRef(null);
+    const refSerie = useRef(null);
+    const refColor = useRef(null);
+    const refEstado = useRef(null);
+
+    const envioForm = () => {
+        
+        const data = {
+            user: {
+                user: refUser.current.value,
+                cc: refCC.current.value,
+                email: refEmail.current.value,
+                pass: refPass.current.value
+            },
+            bike: {
+                marca: refMarca.current.value,
+                serie: refSerie.current.value,
+                color: refColor.current.value,
+                estado: refEstado.current.value
+            },
+        }
+
+        console.log("datos a enviar",data);
+
+        sendForm(data);  
+    }
+
     const _home = () => {
         return  <div className="col-md-12">
                     <div className="col-md-6">
                         <div className="form-group">
-                            <input className="form-control" placeholder="Nombre"/>
+                            <input className="form-control" placeholder="Nombre" ref={refUser}/>
                         </div>
                         <div className="form-group">
-                            <input className="form-control" placeholder="Apellido"/>
+                            <input className="form-control" placeholder="CC" ref={refCC}/>
                         </div>
                         <div className="form-group">
-                            <input className="form-control" placeholder="CC"/>
+                            <input className="form-control" placeholder="Email" ref={refEmail}/>
                         </div>
                         <div className="form-group">
-                            <input className="form-control" placeholder="Email"/>
-                        </div>
-                        <div className="form-group">
-                            <input className="form-control" type="password" placeholder="Contraseña"/>
+                            <input className="form-control" type="password" placeholder="Contraseña" ref={refPass}/>
                         </div>
                     </div>
                     <div className="col-md-6">
                         <div className="form-group">
-                            <input className="form-control" placeholder="Marca"/>
+                            <input className="form-control" placeholder="Marca" ref={refMarca}/>
                         </div>
                         <div className="form-group">
-                            <input className="form-control" placeholder="Serie"/>
+                            <input className="form-control" placeholder="Serie" ref={refSerie}/>
                         </div>
                         <div className="form-group">
-                            <input className="form-control" placeholder="Color"/>
+                            <input className="form-control" placeholder="Color" ref={refColor}/>
                         </div>
                         <div className="form-group">
-                            <input className="form-control" placeholder="Referencia"/>
-                        </div>
-                        <div className="form-group">
-                            <select className="form-control" placeholder="Estado">
+                            <select className="form-control" placeholder="Estado" ref={refEstado}>
                                 <option value="Sin novedad">Sin novedad</option>
                                 <option value="Robada">Robada</option>
                             </select>
@@ -88,7 +117,8 @@ export default function register()
                     }
                     <div className="col-md-12">
                         {/* lo hice de esta manera ya que tenia que resetear la url para cuando ingrese a welcome */}
-                        <button className="btn btn-info" onClick={clickbtn}>Registrarse</button>
+                        {/* <button className="btn btn-info" onClick={clickbtn}>Registrarse</button> */}
+                        <button className="btn btn-info" onClick={envioForm}>Registrarse</button>
                         {/* <Link className="btn btn-info" to="/" onClick={clickbtn}>Registrarse</Link> */}
                     </div>
                 </form>
